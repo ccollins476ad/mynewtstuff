459ccce7651bd0d1e1bc1514ee624b562d2f9188
Christopher Collins <ccollins@apache.org>
2020-05-19 15:47:22 -0700

    apps/tcp_oic

---
+++ apps/tcp_oic/src/main.c
@@ -0,0 +1,157 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "os/mynewt.h"
+#include <bsp/bsp.h>
+#include <hal/hal_gpio.h>
+#include <console/console.h>
+#include <assert.h>
+#include <string.h>
+
+#ifdef ARCH_sim
+#include <mcu/mcu_sim.h>
+#endif
+
+#include "mn_socket/mn_socket.h"
+#include "oic/port/mynewt/tcp4.h"
+#include "oic/oc_api.h"
+
+/* Task 1 */
+#define TASK1_PRIO (8)
+#define TASK1_STACK_SIZE    OS_STACK_ALIGN(512)
+static os_stack_t task1_stack[TASK1_STACK_SIZE];
+static struct os_task task1;
+static volatile int g_task1_loops;
+
+/* For LED toggling */
+static int g_led_pin;
+
+static struct mn_socket *sock;
+
+static void
+on_conn_err(struct mn_socket *s, int status, void *arg)
+{
+    console_printf("on_conn_err: status=%d\n", status);
+    assert(s == sock);
+}
+
+static void
+sock_writable(void *cb_arg, int err)
+{
+    int rc;
+
+    rc = oc_tcp4_add_conn(sock, on_conn_err, NULL);
+    assert(rc == 0);
+}
+
+union mn_socket_cb sock_cbs = {
+    .socket.readable = NULL,
+    .socket.writable = sock_writable,
+};
+
+static void
+task1_handler(void *arg)
+{
+    const char *addr = "127.0.0.1";
+    const uint16_t port = 777;
+
+    struct os_task *t;
+    int rc;
+
+    /* Set the led pin for the E407 devboard */
+    g_led_pin = LED_BLINK_PIN;
+    hal_gpio_init_out(g_led_pin, 1);
+
+    rc = mn_socket(&sock, MN_PF_INET, MN_SOCK_STREAM, 0);
+    assert(rc == 0);
+
+    sock->ms_cbs = &sock_cbs;
+
+    struct mn_sockaddr_in sin = {
+        .msin_len = sizeof (struct mn_sockaddr_in),
+        .msin_family = MN_PF_INET,
+        .msin_port = htons(port),
+    };
+
+    rc = mn_inet_pton(MN_PF_INET, addr, &sin.msin_addr);
+    assert(rc == 1);
+
+    mn_close(sock);
+
+    console_printf("connecting to %s\n", addr);
+    rc = mn_connect(sock, (struct mn_sockaddr *)&sin);
+    assert(rc == 0);
+    console_printf("connected to %s\n", addr);
+
+    while (1) {
+        t = os_sched_get_current_task();
+        assert(t->t_func == task1_handler);
+
+        ++g_task1_loops;
+
+        /* Wait one second */
+        os_time_delay(OS_TICKS_PER_SEC);
+    }
+}
+
+/*
+ * OIC platform/resource registration.
+ */
+static void
+omgr_app_init(void)
+{
+    oc_init_platform("MyNewt", NULL, NULL);
+}
+
+static const oc_handler_t omgr_oc_handler = {
+    .init = omgr_app_init,
+};
+
+/**
+ * main
+ *
+ * The main task for the project. This function initializes the packages, calls
+ * init_tasks to initialize additional tasks (and possibly other objects),
+ * then starts serving events from default event queue.
+ *
+ * @return int NOTE: this function should never return!
+ */
+int
+main(int argc, char **argv)
+{
+    int rc;
+
+#ifdef ARCH_sim
+    mcu_sim_parse_args(argc, argv);
+#endif
+
+    sysinit();
+
+    os_task_init(&task1, "task1", task1_handler, NULL,
+            TASK1_PRIO, OS_WAIT_FOREVER, task1_stack, TASK1_STACK_SIZE);
+
+    oc_main_init((oc_handler_t *)&omgr_oc_handler);
+
+    while (1) {
+        os_eventq_run(os_eventq_dflt_get());
+    }
+    /* Never returns */
+
+    return rc;
+}
